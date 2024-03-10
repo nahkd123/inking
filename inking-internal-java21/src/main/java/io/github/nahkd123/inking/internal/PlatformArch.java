e6ed73c103b4558d99086d48483983cddda8c7a9
An <nahkd123@gmail.com>
2024-03-10 20:51:14 +0700

    [ci skip] Create ci.yml workflow (#7)
    * Create ci.yml workflow
    
    Linux only atm because I need to confirm that it is working on Linux.
    
    * Add sudo
    
    * Tap into directory structure to debug
    
    * Ok yep I forgot to checkout
    
    * Fix RID not being extracted on system that isn't Winows
    
    * I hope this will works (for arm64)
    
    * Ok yes I am not good at Bash
    
    * Totally forgot the zlib1g lol
    
    * Add Windows and OSX natives to workflow
    
    * Use shasum instead of sha1sum
    
    * Add Cygwin + Mingw to Windows Natives job
    
    * Remove Cygwin + Mingw because make.exe might exists
    
    * Add make and shasum packages from cygwin
    
    * Add the final Java build part
    
    * Use Eclipse Temurin 21
    
    * Remove * symbols in path
    
    * Detect aarch64 as arm64
    
    * Actually return null if native does not exists

--- inking-internal-java21/src/main/java/io/github/nahkd123/inking/internal/PlatformArch.java
+++ inking-internal-java21/src/main/java/io/github/nahkd123/inking/internal/PlatformArch.java
@@ -1,25 +1,27 @@
 package io.github.nahkd123.inking.internal;
 
 public enum PlatformArch {
-	X86_64("amd64", "x64"),
-	ARM64("arm64", "arm64"),
-	UNKNOWN("unknown", "unknown");
+	X86_64(new String[] { "amd64" }, "x64"),
+	ARM64(new String[] { "arm64", "aarch64" }, "arm64"),
+	UNKNOWN(new String[] { "unknown" }, "unknown");
 
-	private String systemPropValue;
+	private String[] systemPropValues;
 	private String archId;
 
-	private PlatformArch(String systemPropValue, String archId) {
-		this.systemPropValue = systemPropValue;
+	private PlatformArch(String[] systemPropValues, String archId) {
+		this.systemPropValues = systemPropValues;
 		this.archId = archId;
 	}
 
-	public String getSystemPropValue() { return systemPropValue; }
+	public String[] getSystemPropValues() { return systemPropValues; }
 
 	public String getArchId() { return archId; }
 
 	public static PlatformArch getCurrent() {
 		String name = System.getProperty("os.arch");
-		for (PlatformArch arch : values()) if (name.equals(arch.systemPropValue)) return arch;
+		for (PlatformArch arch : values()) {
+			for (String sysProp : arch.systemPropValues) if (name.equals(sysProp)) return arch;
+		}
 		return UNKNOWN;
 	}
 }
