1d186e28f9cab52c22a6ae29c1f39e2f337526bb
nahkd123 <nahkd123@gmail.com>
2024-08-11 17:45:33 +0700

    Java 22

--- inking-otd/src/main/java/io/github/nahkd123/inking/otd/netnative/OtdNative.java
+++ inking-otd/src/main/java/io/github/nahkd123/inking/otd/netnative/OtdNative.java
@@ -21,130 +21,130 @@ import io.github.nahkd123.inking.internal.PlatformUtils;
 import io.github.nahkd123.inking.otd.internal.SimpleLoggerAdapter;
 
 public class OtdNative {
-	private static final Logger LOGGER = LoggerFactory.getLogger("Inking/OpenTabletDriver/NativeLoader");
+    private static final Logger LOGGER = LoggerFactory.getLogger("Inking/OpenTabletDriver/NativeLoader");
 
-	private Linker linker;
-	private SymbolLookup lib;
+    private Linker linker;
+    private SymbolLookup lib;
 
-	private MethodHandle initializeDriverHandle;
-	private MethodHandle getTabletInfoHandle;
-	private Arena arena;
+    private MethodHandle initializeDriverHandle;
+    private MethodHandle getTabletInfoHandle;
+    private Arena arena;
 
-	public OtdNative(Linker linker, SymbolLookup lib, Arena arena) {
-		this.linker = linker;
-		this.lib = lib;
-		this.arena = arena;
+    public OtdNative(Linker linker, SymbolLookup lib, Arena arena) {
+        this.linker = linker;
+        this.lib = lib;
+        this.arena = arena;
 
-		initializeDriverHandle = linker.downcallHandle(
-			lib.find("initialize_driver").get(),
-			FunctionDescriptor.of(
-				ValueLayout.JAVA_BOOLEAN,
-				ValueLayout.ADDRESS,
-				ValueLayout.ADDRESS));
-		getTabletInfoHandle = linker.downcallHandle(
-			lib.find("get_tablet_info").get(),
-			FunctionDescriptor.of(
-				ValueLayout.JAVA_BOOLEAN,
-				stringLayout(),
-				ValueLayout.ADDRESS.withTargetLayout(OtdTabletSpec.layout())));
-	}
+        initializeDriverHandle = linker.downcallHandle(
+            lib.find("initialize_driver").get(),
+            FunctionDescriptor.of(
+                ValueLayout.JAVA_BOOLEAN,
+                ValueLayout.ADDRESS,
+                ValueLayout.ADDRESS));
+        getTabletInfoHandle = linker.downcallHandle(
+            lib.find("get_tablet_info").get(),
+            FunctionDescriptor.of(
+                ValueLayout.JAVA_BOOLEAN,
+                stringLayout(),
+                ValueLayout.ADDRESS.withTargetLayout(OtdTabletSpec.layout())));
+    }
 
-	/**
-	 * <p>
-	 * Attempt to find native library and load it as {@link OtdNative}. This will
-	 * attempts to copy native library from JAR file to a directory.
-	 * </p>
-	 * 
-	 * @param copyDest       Copy destination.
-	 * @param linker         The native linker.
-	 * @param arena          The arena.
-	 * @param ignoreChecksum Ignore checksum, allowing user to use their own native
-	 *                       library.
-	 * @return The library, or {@code null} if no suitable library can be found.
-	 */
-	public static OtdNative findNative(Path copyDest, Linker linker, Arena arena, boolean ignoreChecksum) {
-		ClassLoader clsLoader = OtdNative.class.getClassLoader();
-		SymbolLookup lib = PlatformUtils.loadLibrary("Inking.Otd", clsLoader, copyDest, arena,
-			new SimpleLoggerAdapter(LOGGER), ignoreChecksum);
-		return lib != null ? new OtdNative(linker, lib, arena) : null;
-	}
+    /**
+     * <p>
+     * Attempt to find native library and load it as {@link OtdNative}. This will
+     * attempts to copy native library from JAR file to a directory.
+     * </p>
+     * 
+     * @param copyDest       Copy destination.
+     * @param linker         The native linker.
+     * @param arena          The arena.
+     * @param ignoreChecksum Ignore checksum, allowing user to use their own native
+     *                       library.
+     * @return The library, or {@code null} if no suitable library can be found.
+     */
+    public static OtdNative findNative(Path copyDest, Linker linker, Arena arena, boolean ignoreChecksum) {
+        ClassLoader clsLoader = OtdNative.class.getClassLoader();
+        SymbolLookup lib = PlatformUtils.loadLibrary("Inking.Otd", clsLoader, copyDest, arena,
+            new SimpleLoggerAdapter(LOGGER), ignoreChecksum);
+        return lib != null ? new OtdNative(linker, lib, arena) : null;
+    }
 
-	/**
-	 * <p>
-	 * Find native library, load it as {@link OtdNative}. Basically
-	 * {@link #findNative(Path, Linker, Arena, boolean)}, but for applications that
-	 * doesn't want to enable preview features. This will use native linker +
-	 * {@link Arena#ofAuto()}.
-	 * </p>
-	 * 
-	 * @param copyDest       Copy destination.
-	 * @param ignoreChecksum Ignore checksum, allowing user to use their own native
-	 *                       library.
-	 * @return The library, or {@code null} if no suitable library can be found.
-	 */
-	public static OtdNative findNative(Path copyDest, boolean ignoreChecksum) {
-		return findNative(copyDest, Linker.nativeLinker(), Arena.ofAuto(), ignoreChecksum);
-	}
+    /**
+     * <p>
+     * Find native library, load it as {@link OtdNative}. Basically
+     * {@link #findNative(Path, Linker, Arena, boolean)}, but for applications that
+     * doesn't want to enable preview features. This will use native linker +
+     * {@link Arena#ofAuto()}.
+     * </p>
+     * 
+     * @param copyDest       Copy destination.
+     * @param ignoreChecksum Ignore checksum, allowing user to use their own native
+     *                       library.
+     * @return The library, or {@code null} if no suitable library can be found.
+     */
+    public static OtdNative findNative(Path copyDest, boolean ignoreChecksum) {
+        return findNative(copyDest, Linker.nativeLinker(), Arena.ofAuto(), ignoreChecksum);
+    }
 
-	protected static AddressLayout stringLayout() {
-		return ValueLayout.ADDRESS.withTargetLayout(MemoryLayout.sequenceLayout(ValueLayout.JAVA_BYTE));
-	}
+    protected static AddressLayout stringLayout() {
+        return ValueLayout.ADDRESS.withTargetLayout(MemoryLayout.sequenceLayout(256L, ValueLayout.JAVA_BYTE));
+    }
 
-	public Linker getLinker() { return linker; }
+    public Linker getLinker() { return linker; }
 
-	public SymbolLookup getLib() { return lib; }
+    public SymbolLookup getLib() { return lib; }
 
-	public Arena getArena() { return arena; }
+    public Arena getArena() { return arena; }
 
-	public void initializeDriver(BiConsumer<String, Boolean> connectStateCallback, BiConsumer<String, OtdPacket> packetCallback) {
-		try {
-			MethodHandle connectFuncHandle = MethodHandles.lookup().bind(
-				(ConnectStateCallbackBridge) (serialStr, connect) -> {
-					String serial = serialStr.getUtf8String(0L);
-					connectStateCallback.accept(serial, connect);
-				}, "onConnect", MethodType.methodType(Void.TYPE, MemorySegment.class, boolean.class));
-			MethodHandle packetFuncHandle = MethodHandles.lookup().bind(
-				(PacketCallbackBridge) (serialStr, packetMemory) -> {
-					String serial = serialStr.getUtf8String(0L);
-					OtdPacket packet = new OtdPacket(packetMemory);
-					packetCallback.accept(serial, packet);
-				}, "onPacket", MethodType.methodType(Void.TYPE, MemorySegment.class, MemorySegment.class));
+    public void initializeDriver(BiConsumer<String, Boolean> connectStateCallback, BiConsumer<String, OtdPacket> packetCallback) {
+        try {
+            MethodHandle connectFuncHandle = MethodHandles.lookup().bind(
+                (ConnectStateCallbackBridge) (serialStr, connect) -> {
+                    String serial = serialStr.getString(0L);
+                    connectStateCallback.accept(serial, connect);
+                }, "onConnect", MethodType.methodType(Void.TYPE, MemorySegment.class, boolean.class));
+            MethodHandle packetFuncHandle = MethodHandles.lookup().bind(
+                (PacketCallbackBridge) (serialStr, packetMemory) -> {
+                    String serial = serialStr.getString(0L);
+                    OtdPacket packet = new OtdPacket(packetMemory);
+                    packetCallback.accept(serial, packet);
+                }, "onPacket", MethodType.methodType(Void.TYPE, MemorySegment.class, MemorySegment.class));
 
-			MemorySegment connectFunc = linker.upcallStub(
-				connectFuncHandle,
-				FunctionDescriptor.ofVoid(stringLayout(), ValueLayout.JAVA_BOOLEAN),
-				arena);
-			MemorySegment packetFunc = linker.upcallStub(
-				packetFuncHandle,
-				FunctionDescriptor.ofVoid(stringLayout(), OtdPacket.layout()),
-				arena);
+            MemorySegment connectFunc = linker.upcallStub(
+                connectFuncHandle,
+                FunctionDescriptor.ofVoid(stringLayout(), ValueLayout.JAVA_BOOLEAN),
+                arena);
+            MemorySegment packetFunc = linker.upcallStub(
+                packetFuncHandle,
+                FunctionDescriptor.ofVoid(stringLayout(), OtdPacket.layout()),
+                arena);
 
-			boolean result = (boolean) initializeDriverHandle.invoke(connectFunc, packetFunc);
-			if (!result) throw new RuntimeException("Driver is already initialized or failed to initialize");
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		}
-	}
+            boolean result = (boolean) initializeDriverHandle.invoke(connectFunc, packetFunc);
+            if (!result) throw new RuntimeException("Driver is already initialized or failed to initialize");
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public OtdTabletSpec getTabletInfo(String serial) {
-		try {
-			MemorySegment serialStr = arena.allocateUtf8String(serial);
-			MemorySegment specPtr = arena.allocate(OtdTabletSpec.layout());
-			boolean result = (boolean) getTabletInfoHandle.invoke(serialStr, specPtr);
-			if (!result) return null;
-			return new OtdTabletSpec(specPtr);
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public OtdTabletSpec getTabletInfo(String serial) {
+        try {
+            MemorySegment serialStr = arena.allocateFrom(serial);
+            MemorySegment specPtr = arena.allocate(OtdTabletSpec.layout());
+            boolean result = (boolean) getTabletInfoHandle.invoke(serialStr, specPtr);
+            if (!result) return null;
+            return new OtdTabletSpec(specPtr);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@FunctionalInterface
-	private static interface ConnectStateCallbackBridge {
-		void onConnect(MemorySegment serialStr, boolean connected);
-	}
+    @FunctionalInterface
+    private static interface ConnectStateCallbackBridge {
+        void onConnect(MemorySegment serialStr, boolean connected);
+    }
 
-	@FunctionalInterface
-	private static interface PacketCallbackBridge {
-		void onPacket(MemorySegment serialStr, MemorySegment packet);
-	}
+    @FunctionalInterface
+    private static interface PacketCallbackBridge {
+        void onPacket(MemorySegment serialStr, MemorySegment packet);
+    }
 }
--- inking-otd/src/main/java/io/github/nahkd123/inking/otd/netnative/OtdTabletSpec.java
+++ inking-otd/src/main/java/io/github/nahkd123/inking/otd/netnative/OtdTabletSpec.java
@@ -11,54 +11,54 @@ import io.github.nahkd123.inking.api.util.MeasurementUnit;
 import io.github.nahkd123.inking.api.util.Vector2;
 
 public class OtdTabletSpec implements TabletInfo {
-	private String tabletName;
-	private int maxPressure;
-	private Optional<Vector2> physicalSize;
-	private Optional<Vector2> inputSize;
-	private int penButtons;
-	private int auxButtons;
+    private String tabletName;
+    private int maxPressure;
+    private Optional<Vector2> physicalSize;
+    private Optional<Vector2> inputSize;
+    private int penButtons;
+    private int auxButtons;
 
-	public OtdTabletSpec(MemorySegment memory) {
-		tabletName = memory.get(OtdNative.stringLayout(), 0L).getUtf8String(0L);
-		maxPressure = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize());
-		float pW = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 4L);
-		float pH = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 8L);
-		float iW = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 12L);
-		float iH = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 16L);
-		physicalSize = Optional.of(new Vector2(pW, pH, MeasurementUnit.MILLIMETER));
-		inputSize = Optional.of(new Vector2(iW, iH));
-		penButtons = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize() + 20L);
-		auxButtons = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize() + 24L);
-	}
+    public OtdTabletSpec(MemorySegment memory) {
+        tabletName = memory.get(OtdNative.stringLayout(), 0L).getString(0L);
+        maxPressure = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize());
+        float pW = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 4L);
+        float pH = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 8L);
+        float iW = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 12L);
+        float iH = memory.get(ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS.byteSize() + 16L);
+        physicalSize = Optional.of(new Vector2(pW, pH, MeasurementUnit.MILLIMETER));
+        inputSize = Optional.of(new Vector2(iW, iH));
+        penButtons = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize() + 20L);
+        auxButtons = memory.get(ValueLayout.JAVA_INT, ValueLayout.ADDRESS.byteSize() + 24L);
+    }
 
-	@Override
-	public String getTabletName() { return tabletName; }
+    @Override
+    public String getTabletName() { return tabletName; }
 
-	@Override
-	public int getMaxPressure() { return maxPressure; }
+    @Override
+    public int getMaxPressure() { return maxPressure; }
 
-	@Override
-	public Optional<Vector2> getPhysicalSize() { return physicalSize; }
+    @Override
+    public Optional<Vector2> getPhysicalSize() { return physicalSize; }
 
-	@Override
-	public Optional<Vector2> getInputSize() { return inputSize; }
+    @Override
+    public Optional<Vector2> getInputSize() { return inputSize; }
 
-	@Override
-	public int getButtonsCount(ButtonType type) {
-		return switch (type) {
-		case PEN -> penButtons;
-		case AUXILIARY -> auxButtons;
-		default -> 0;
-		};
-	}
+    @Override
+    public int getButtonsCount(ButtonType type) {
+        return switch (type) {
+        case PEN -> penButtons;
+        case AUXILIARY -> auxButtons;
+        default -> 0;
+        };
+    }
 
-	public static MemoryLayout layout() {
-		return MemoryLayout.structLayout(
-			OtdNative.stringLayout().withName("tabletName"),
-			ValueLayout.JAVA_INT.withName("maxPressure"),
-			MemoryLayout.sequenceLayout(2L, ValueLayout.JAVA_FLOAT).withName("physicalSize"),
-			MemoryLayout.sequenceLayout(2L, ValueLayout.JAVA_FLOAT).withName("inputSize"),
-			ValueLayout.JAVA_INT.withName("penButtons"),
-			ValueLayout.JAVA_INT.withName("auxButtons"));
-	}
+    public static MemoryLayout layout() {
+        return MemoryLayout.structLayout(
+            OtdNative.stringLayout().withName("tabletName"),
+            ValueLayout.JAVA_INT.withName("maxPressure"),
+            MemoryLayout.sequenceLayout(2L, ValueLayout.JAVA_FLOAT).withName("physicalSize"),
+            MemoryLayout.sequenceLayout(2L, ValueLayout.JAVA_FLOAT).withName("inputSize"),
+            ValueLayout.JAVA_INT.withName("penButtons"),
+            ValueLayout.JAVA_INT.withName("auxButtons"));
+    }
 }
